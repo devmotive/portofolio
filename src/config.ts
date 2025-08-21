3c6922ac251db0b983b1405c6bf41d94c4ca88b9
devmotive <ahmad.codespace@gmail.com>
2025-08-21 12:05:58 +0700

    Update social media links and email in site configuration

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,10 @@ export const siteConfig = {
   description: "Portfolio website of Achmad Syeful",
   accentColor: "#ef4444",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "ahmad.codespace@gmail.com",
+    linkedin: "https://linkedin.com/in/achmad-syeful",
+    twitter: "",
+    github: "https://github.com/devmotive",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
